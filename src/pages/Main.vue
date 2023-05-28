cb568c66b78a5558edef44dd65bbf41185587e72
vas1levnv <vas1levnv@yandex.ru>
2023-05-28 20:20:09 +0300

    верстка

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,5 +1,19 @@
 <template>
-<h1>main</h1>
+    <form class="main-form">
+        <h1 class="main-form-title">Персональные данные</h1>
+
+        <label class="main-form-label" for="name">
+            <div class="main-form-label-text">Имя</div>
+            <input id="name" type="text" placeholder="Петр">
+        </label>
+
+      <label class="main-form-label" for="name">
+        <div class="main-form-label-text">Возраст</div>
+        <input id="name" type="text" placeholder="99">
+      </label>
+
+
+    </form>
 </template>
 
 <script>
@@ -9,5 +23,36 @@ export default {
 </script>
 
 <style scoped>
+
+.main-form {
+    margin: 30px 0;
+}
+
+.main-form-title{
+  font-weight: 500;
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+
+
+.main-form-label{
+    display: block;
+    width: 100%;
+    border: 1px solid #F1F1F1;
+    border-radius: 4px;
+    padding: 8px 16px;
+    cursor: pointer;
+  margin-bottom: 10px;
+}
+
+.main-form-label-text{
+  color: rgba(17, 17, 17, 0.48);
+}
+
+input{
+width: 100%;
+  outline: none;
+  border: none;
+}
 
 </style>
