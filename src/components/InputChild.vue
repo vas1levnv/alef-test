a8d0c81d4e37b4fd3e53729acf9c7db0115af8f1
vas1levnv <vas1levnv@yandex.ru>
2023-05-29 13:11:14 +0300

    компонента детей

---
+++ src/components/InputChild.vue
@@ -0,0 +1,49 @@
+<template>
+    <div :id="child.id">
+        <label  class="main-form-label" for="childName">
+            <div v-show="isErrorName" style="color: red">Введите имя</div>
+            <div class="main-form-label-text">Имя</div>
+            <input @blur="errorNameBlur"
+                   @focus="isErrorName = false"
+                   v-model="childName"
+                   id="childName" type="text" placeholder="Петр">
+        </label>
+
+        <label class="main-form-label" for="childAge">
+            <div v-show="isErrorAge" style="color: red">Введите возраст</div>
+            <div class="main-form-label-text">Возраст</div>
+            <input
+                @blur="errorAgeBlur"
+                @focus="isErrorAge = false"
+                v-model="childAge" id="childAge" type="number" placeholder="99">
+        </label>
+        <button @click="$emit('remove', child.id)">Удалить</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['child'],
+    name: "InputChild",
+    data() {
+        return {
+            childName: null,
+            childAge: null,
+            isErrorName: false,
+            isErrorAge: false,
+        }
+    },
+    methods:{
+        errorNameBlur(){
+            !this.childName ? this.isErrorName = true : false
+        },
+        errorAgeBlur(){
+            !this.childAge ? this.isErrorAge = true : false
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,58 +1,95 @@
 <template>
-    <form class="main-form">
+    <form @submit.prevent class="main-form">
         <h1 class="main-form-title">Персональные данные</h1>
 
         <label class="main-form-label" for="name">
             <div class="main-form-label-text">Имя</div>
-            <input id="name" type="text" placeholder="Петр">
+            <input v-model="name" id="name" type="text" placeholder="Петр">
         </label>
 
-      <label class="main-form-label" for="name">
-        <div class="main-form-label-text">Возраст</div>
-        <input id="name" type="text" placeholder="99">
-      </label>
+        <label class="main-form-label" for="name">
+            <div class="main-form-label-text">Возраст</div>
+            <input v-model="age" id="age" type="number" placeholder="99">
+        </label>
 
+        <div v-show="children.length < 5">
+            <button @click="addChild">+ Добавить ребенка</button>
+        </div>
+        <div>
+            Дети (макс. 5)
+        </div>
+        <InputChild v-for="child in children"
+                    @remove="removeChild"
+                    :key="child.id" v-bind:child="child"
+        />
+        <button @click="saveForm">
+            Сохранить
+        </button>
 
     </form>
 </template>
 
 <script>
+import InputChild from "@/components/InputChild.vue";
+
 export default {
-    name: "Main"
+    name: "Main",
+    components: {InputChild},
+    data() {
+        return {
+            name: null,
+            age: null,
+
+            childInc: 0,
+            children: [],
+        }
+    },
+    methods: {
+        addChild() {
+            this.childInc += 1
+            this.children.push({id: this.childInc})
+        },
+        removeChild(id) {
+            this.children = this.children.filter(c => c.id !== id)
+        },
+        saveForm() {
+            console.log(this.name, this.age)
+        },
+    }
 }
 </script>
 
-<style scoped>
+<style>
 
 .main-form {
     margin: 30px 0;
 }
 
-.main-form-title{
-  font-weight: 500;
-  font-size: 16px;
-  margin-bottom: 20px;
+.main-form-title {
+    font-weight: 500;
+    font-size: 16px;
+    margin-bottom: 20px;
 }
 
 
-.main-form-label{
+.main-form-label {
     display: block;
     width: 100%;
     border: 1px solid #F1F1F1;
     border-radius: 4px;
     padding: 8px 16px;
     cursor: pointer;
-  margin-bottom: 10px;
+    margin-bottom: 10px;
 }
 
-.main-form-label-text{
-  color: rgba(17, 17, 17, 0.48);
+.main-form-label-text {
+    color: rgba(17, 17, 17, 0.48);
 }
 
-input{
-width: 100%;
-  outline: none;
-  border: none;
+input {
+    width: 100%;
+    outline: none;
+    border: none;
 }
 
 </style>
